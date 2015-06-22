e92bcc0638393cffafa53de0d8ac6e51c16e8d41
Aleksander Gurin <alek.gurin@gmail.com>
2015-06-23 00:52:12 +0300

    Cleanup in setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,7 @@
 #!/usr/bin/env python
 
-import os
-import sys
+from distutils.core import setup
 
-from distutils.core import setup, Command
-
-# if sys.argv[-1] == 'publish':
-#     os.system('python setup.py sdist upload')
-#     os.system('python setup.py bdist_wheel upload')
-#     sys.exit()
-#
-# class PyTest(Command):
-#     user_options = []
-#
-#     def initialize_options(self):
-#         pass
-#
-#     def finalize_options(self):
-#         pass
-#
-#     def run(self):
-#         import pytest
-#         errno = pytest.main(["tests.py"])
-#         sys.exit(errno)
 
 def long_description():
     with open('README') as f:
@@ -31,7 +10,7 @@ def long_description():
 setup(
     name='son',
     version='0.1',
-    description='Simple Object Notation data interchange format.',
+    description='(SON) Simple Object Notation data interchange format.',
     long_description=long_description(),
     author='Aleksander Gurin',
     author_email='alek.gurin@gmail.com',
@@ -39,9 +18,6 @@ setup(
     py_modules=['son'],
     license='Public Domain',
     keywords='simple object notation, data format, serialization, deserialization',
-    # cmdclass={'test': PyTest},
-    # test_suite='tests',
-    # tests_require='pytest',
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
